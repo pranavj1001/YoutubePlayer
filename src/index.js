ee942d7958a604af0ae76b94c87847a333bd101b
pranavj1001 <pranavj1001@gmail.com>
2018-03-06 10:10:42 +0530

    add constructor to take get initial list of videos

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
-YTSearch({ key: API_KEY, term: 'pewdewpie' }, data => console.log(data));
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { youtubeVideos: [] };
+    YTSearch({ key: API_KEY, term: 'pewdewpie' },
+      youtubeVideos => this.setState({ youtubeVideos }));
+  }
 
   render() {
     return (
