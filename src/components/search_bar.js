b3e7daa177c99fcdb4e491545242e5454593859f
pranavj1001 <pranavj1001@gmail.com>
2018-03-10 15:35:26 +0530

    add logic to search for videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,12 +7,17 @@ class SearchBar extends Component {
     this.state = { query: '' };
   }
 
+  onInputChange(query) {
+    this.setState({ query });
+    this.props.onSearchTermChange(query);
+  }
+
   render() {
     return (
       <div className="search-bar">
         <input
           value={this.state.query}
-          onChange={event => this.setState({ query: event.target.value })}
+          onChange={event => this.onInputChange(event.target.value)}
         />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: 'subnautica' },
+    this.videoSearch('youtube');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term },
       (youtubeVideos) => {
         this.setState({
           youtubeVideos,
@@ -27,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
