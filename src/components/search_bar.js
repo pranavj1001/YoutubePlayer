0083fc28cd4352d8dc6c43988b229eb53ad7b940
pranavj1001 <pranavj1001@gmail.com>
2018-03-11 15:15:54 +0530

    add animation for search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -16,6 +16,7 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
+          placeholder="Search.."
           value={this.state.query}
           onChange={event => this.onInputChange(event.target.value)}
         />
