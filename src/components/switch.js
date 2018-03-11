b54e179af17d177e0aca58e1c789f03c73814f6a
pranavj1001 <pranavj1001@gmail.com>
2018-03-11 17:24:29 +0530

    add logic to change state

--- src/components/switch.js
+++ src/components/switch.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class Switch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isChecked: true,
+    };
+  }
+
   render() {
     return(
       <div className="control--switch">
         <label className="switch">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={this.state.isChecked}
+            onChange={() => {
+              this.setState({ isChecked: !this.state.isChecked });
+              this.props.onNightModeChanged(this.state.isChecked);
+            }}
+          />
           <span className="slider round" />
         </label>
         <p>Day/Night</p>
