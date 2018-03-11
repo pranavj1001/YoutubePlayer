0b2d184f2d60bf0c305c8719ed35905810aca3c3
pranavj1001 <pranavj1001@gmail.com>
2018-03-11 17:25:54 +0530

    manage states and colorScheme at root component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import Switch from './components/switch';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
+const API_KEY = ''; // Enter API_KEY here
 
 class App extends Component {
 
@@ -13,8 +16,12 @@ class App extends Component {
 
     this.state = {
       youtubeVideos: [],
-      selectedVideo: null
+      selectedVideo: null,
+      listColor: 'list-group-item list-group-item-dark'
     };
+
+    document.querySelector('body').style.backgroundColor = '#333';
+    document.querySelector('body').style.color = '#fff';
 
     this.videoSearch('youtube');
   }
@@ -29,6 +36,18 @@ class App extends Component {
       });
   }
 
+  viewModeChanged(mode) {
+    if (mode) {
+      document.querySelector('body').style.backgroundColor = '#fff';
+      document.querySelector('body').style.color = '#333';
+      this.setState({ listColor: 'list-group-item list-group-item-light' });
+    } else {
+      document.querySelector('body').style.backgroundColor = '#333';
+      document.querySelector('body').style.color = '#fff';
+      this.setState({ listColor: 'list-group-item list-group-item-dark' });
+    }
+  }
+
   render() {
     // call this function after every 300ms
     const videoSearch = _.debounce((term) => { this.videoSearch(term); }, 300);
@@ -37,9 +56,11 @@ class App extends Component {
       <div>
         <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
+        <Switch onNightModeChanged={mode => this.viewModeChanged(mode)} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
           videosList={this.state.youtubeVideos}
+          listColor={this.state.listColor}
         />
       </div>
     );
